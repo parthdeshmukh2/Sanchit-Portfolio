350d2e0b9d354c30754a932675f340829cf6bca0
Parth Deshmukh <97448096+parthdeshmukh2@users.noreply.github.com>
2024-09-12 18:29:49 +0530

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import profile from "../../utils/images/profile.jpeg"
+import profile from "../utils/images/profile.jpeg"'
 
 const About = () => {
     return (
